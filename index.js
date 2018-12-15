ec87fe3dd78f3def8afbe818661db3c2d294e5ef
austinbillings <austin@austinbillings.com>
2018-12-15 02:01:57 -0500

    pull in new utils and constants and export methods from trolley.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('./src/Trolley');
+const utils = require('./src/utils')
+const trolley = require('./src/trolley')
+const statuses = require('./src/constants/statuses')
+const statusCodes = require('./src/constants/statusCodes')
+
+
+module.exports = { utils, statuses, statusCodes, ...trolley }
